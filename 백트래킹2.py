537c81db6f273642c4a1cac5066a9dba21e3dff5
P-project-G <pgh94a@naver.com>
2020-10-11 19:29:37 +0900

    baekjoon 백트래킹 #2 N과M(2) 연습

--- 백트래킹2.py
+++ 백트래킹2.py
@@ -1,15 +1,17 @@
-def DFS(d,N,M):
+def DFS(d,idx,N,M):
     if d==M:
         print(*result)
         return
-    for i in range (N):
+    for i in range (idx,N):
         if check[i]==True:
+            print(check)
             continue
+
 
         check[i]=True
         result.append(i+1)
 
-        DFS(d+1,N,M)
+        DFS(d+1,i+1,N,M)
 
         result.pop()
         check[i]=False
@@ -17,4 +19,4 @@ def DFS(d,N,M):
 N,M=map(int,input().split())
 check=[False]*N
 result=[]
-DFS(0,N,M)
+DFS(0,0,N,M)
