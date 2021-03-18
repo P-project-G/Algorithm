3a99c305d57b959d794664c9a54d6b2a10f3b062
project_P <pgh94a@naver.com>
2021-03-19 03:02:45 +0900

    #Startup Coding Festival 2021 #그리디알고리즘 #사은품 교환하기

--- Startup Coding Festival 2021/사은품 교환하기.py
+++ Startup Coding Festival 2021/사은품 교환하기.py
@@ -2,47 +2,45 @@
 # UTF-8 encoding when using korean
 
 
-#T = int(input())  # 테스트케이스의 수
+t = int(input())  # 테스트케이스 수
+for _ in range(t):
+    # N : 시즌한정음료 쿠폰의 수
+    # M : 일반음료 쿠폰의 수
+    N, M = map(int, input().split())
+    cnt = 0  # 구매한 음료의 수
 
-#for _ in range(T):
-cnt = 0  # 구매한 음료의 수
-# N : 시즌한정음료 쿠폰의 수
-# M : 일반음료 쿠폰의 수
-N, M = map(int, input().split())
+    Nmok = N // 5
+    Nmod = N % 5
+    Mmok = M // 7
+    Mmod = M % 7
 
-Nmok=N//5
-Nmod=N%5
-Mmok=M//7
-Mmod=M%7
+    if Nmok > Mmok:
+        cnt += Mmok
+        N = (Nmok - Mmok) * 5 + Nmod
+        M = Mmod
+    elif Nmok <= Mmok:
+        cnt += Nmok
+        N = Nmod
+        M = (Mmok - Nmok) * 7 + Mmod
 
-if Nmok > Mmok:
-    cnt += Mmok
-    N = (Nmok-Mmok)*5 + Nmod
-    M = Mmod
-elif Nmok < Mmok:
-    cnt += Nmok
-    N = Nmod
-    M = (Mmok-Nmok)*7 + Mmod
+    if N < 5:
+        print(cnt)
 
-print(N,M,cnt)
+    else:
+        if N >= 5 and M > 0 and N + M >= 12:
+            N -= 12 - M
+            M = 0
+            cnt += 1
+        if N >= 12:
+            cnt += N // 12
 
-if N<5:
-    print(cnt)
+        print(cnt)
 
-else:
-    if N>=5 and M>0 and N+M>=12:
-        N -= 12-M
-        M = 0
-        cnt += 1
 
-    if N>=12:
-        cnt += N//12
-
-    print(cnt)
 """
-30 20
-
+4
+12 0
+10 14
+4 20
 5 2147483648
-
-108 62
 """
